9332bf03100f946a894e06d6f417e5390ca5cb7f
Jufedev <jufesena@gmail.com>
2026-02-03 17:11:57 -0500

    feat: Add connectivity backend structure

---
+++ accounts/connectivity/variables.tf
@@ -0,0 +1,21 @@
+variable "aws_region" {
+  description = "value"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Nombre del proyecto para Tags"
+  type        = string
+}
+
+
+variable "aws_profile" {
+  description = "Nombre del perfil a usar"
+  type        = string
+}
+
+
+variable "s3_state" {
+  description = "Nombre del perfil a usar"
+  type        = string
+}
